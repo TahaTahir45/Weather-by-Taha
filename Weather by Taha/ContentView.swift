8b834f80c0d07e2f0e575606b31868ce8aa1a235
taha <tahatahir996@gmail.com>
2024-06-02 15:27:00 +0500

    Initial Commit

---
+++ Weather by Taha/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Weather by Taha
+//
+//  Created by Taha Tahir on 6/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Weather by Taha/Weather_by_TahaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_by_TahaApp.swift
+//  Weather by Taha
+//
+//  Created by Taha Tahir on 6/2/24.
+//
+
+import SwiftUI
+
+@main
+struct Weather_by_TahaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
